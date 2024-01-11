07ba8729c0247879f3b0c14e148ffbc4bad20dfd
Arnold-In-Tech <g16a7782@gmail.com>
2024-01-11 04:15:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // Obtain pairs of numbers in the array
+  
+  let i = 0;
+  while (i < array.length){
+
+    for (let indx = 0; indx < array.length-1; indx++){
+
+      // Store current element in a variable
+      let currentElement = array[indx];
+
+      // Get rid of current element from array
+      array.splice(indx, 1)
+
+      // Add current element to each item in array
+      newArray = array.map(item => item + currentElement);
+
+      // Compare the sum of each with the target
+      if (newArray.includes(target)){
+        return true;
+      }
+    }
+    i++;
+  }
+return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0(n)
 */
 
 /* 
   Add your pseudocode here
+
+  Obtain pairs of numbers in the array
+  Add pairs of numbers
+  Compare the sum of each pair with the target
+
 */
 
 /*
   Add written explanation of your solution here
 */
 
+// Test Cases
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
